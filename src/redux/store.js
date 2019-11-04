b4203155b06440491aabe9c7aa4eb35196ca3ddb
Wanwan <wanwanzainaer@gmail.com>
2019-11-04 15:03:26 -0800

    Add persist redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,16 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import reducer from "./reducers";
 import logger from "redux-logger";
-
+import { persistStore } from "redux-persist";
 const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   reducer,
   {},
   composeEnhancers(applyMiddleware(...middlewares))
 );
 
-export default store;
+export const persistor = persistStore(store);
