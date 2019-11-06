8e02541890371ee56d84a48b1b17327ce05efc2b
Wanwan <wanwanzainaer@gmail.com>
2019-11-05 17:32:44 -0800

    updating strip integration

--- src/pages/checkout/CheckOut.js
+++ src/pages/checkout/CheckOut.js
@@ -3,6 +3,7 @@ import "./CheckOut.scss";
 import { createStructuredSelector } from "reselect";
 import { connect } from "react-redux";
 import CheckOutItem from "../../components/checkout-item/CheckOutItem";
+import StripeCheckoutButton from "../../components/stripe-button/StripeButton";
 import {
   selectCartItems,
   selectCartTotal
@@ -33,6 +34,12 @@ const CheckOut = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242-4242-4242-4242 Exp:01/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
