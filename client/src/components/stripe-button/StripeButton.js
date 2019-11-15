62cac1d8e348dd5a0ab1429de9e6293a141747ac
Wanwan <wanwanzainaer@gmail.com>
2019-11-14 22:18:47 -0800

    Add backend for the stripe code and the react componenet to send the information to backend side

--- client/src/components/stripe-button/StripeButton.js
+++ client/src/components/stripe-button/StripeButton.js
@@ -1,12 +1,26 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from "axios";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_eK7lCXNmKPryClNmKjV6nLMp00QrKmGCee";
 
-  const onToken = token => {
-    console.log(token);
+  const onToken = async token => {
+    try {
+      await axios({
+        url: "payment",
+        method: "post",
+        data: {
+          amount: priceForStripe,
+          token
+        }
+      });
+
+      alert("Payment successful");
+    } catch (err) {
+      console.log("Payment error: ", JSON.parse(err));
+      alert("There was an issue with your payment");
+    }
   };
 
   return (
